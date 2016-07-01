d630cc12a028317dcf0776315747a7928437349c
roch4896 <roch4896@colorado.edu>
2016-07-01 14:06:51 -0600

    Update bad_python.py

--- bad_python.py
+++ bad_python.py
@@ -6,4 +6,6 @@ def myfunc(a, b):
 
 a = 1
 
-myfunc(a)
+myfunc(a,b=3)
+#In the original text, there was no value for b, meaning the function will print an error. Thus, you would need to add a value to
+#b in order for "myfunc" to print. It will print 1,3.
